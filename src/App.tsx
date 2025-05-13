73dc5b84c8c3252054e9599b8aa67ff1b92dd144
Mehadi Hasan Reaz <reazmehadihasan@gmail.com>
2025-05-13 23:05:55 +0600

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/reaz-flutter-profile">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/projects" element={<AllProjects />} />
